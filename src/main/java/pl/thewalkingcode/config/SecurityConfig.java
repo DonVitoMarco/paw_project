2516d43b7b1390e642dbbd8dcd97a3f1275f4bb9
Marek Chrapek <mchrapek@omnis.pl>
2017-03-14 16:30:38 +0100

    config security config

--- src/main/java/pl/thewalkingcode/config/SecurityConfig.java
+++ src/main/java/pl/thewalkingcode/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package pl.thewalkingcode.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,5 +12,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @ComponentScan({"pl.thewalkingcode.*"})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/home2").access("hasRole('USER')")
+                .antMatchers("/", "/home").permitAll()
+                .and().formLogin();
+    }
 
 }
